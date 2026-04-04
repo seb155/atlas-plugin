2d317fad18084a5101b4004781c9dccdb65ead5b
atlas-ci <ci@axoiq.com>
2026-04-04 15:02:21 +0000

    chore(release): v4.19.0

--- dist/atlas-admin/scripts/atlas-cli.sh
+++ dist/atlas-admin/scripts/atlas-cli.sh
@@ -11,7 +11,7 @@
 #
 # Modularized: main logic lives in atlas-modules/*.sh
 
-ATLAS_VERSION="4.18.0"
+ATLAS_VERSION="4.19.0"
 ATLAS_CONFIG="${HOME}/.atlas/config.json"
 ATLAS_HISTORY="${HOME}/.atlas/history.json"
 ATLAS_SHELL_DIR="${HOME}/.atlas/shell"
